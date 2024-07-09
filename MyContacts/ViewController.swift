9fe144d1b9918780040a6a4f3f90d8df7e57e3ab
baibhavsingh08 <baibhav.singh@zopsmart.com>
2024-07-09 15:16:35 +0530

    removed unwanted comments

--- MyContacts/ViewController.swift
+++ MyContacts/ViewController.swift
@@ -1,48 +1,11 @@
-//
-//  ViewController.swift
-//  MyContacts
-//
-//  Created by Raramuri on 08/07/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let contacts =  [
-        "John Doe", "Jane Smith", "Michael Johnson", "Emily Brown", "Daniel Wilson",
-        "Sarah Martinez", "Kevin Lee", "Amanda Davis", "Jason Taylor", "Rachel Moore",
-        "David Harris", "Lisa Clark", "James Lewis", "Jessica Walker", "Christopher Hall",
-        "Mary Allen", "Robert Young", "Linda King", "Thomas Wright", "Patricia Scott",
-        "Mark Green", "Barbara Adams", "Steven Baker", "Susan Nelson", "Joseph Hill",
-        "Karen Ramirez", "Charles Campbell", "Nancy Mitchell", "Paul Roberts", "Betty Carter",
-        "Edward Turner", "Margaret Phillips", "Brian Parker", "Sandra Evans", "George Edwards",
-        "Ashley Collins", "Ronald Stewart", "Kimberly Sanchez", "Anthony Morris", "Donna Rogers",
-        "Kenneth Reed", "Michelle Cook", "Jason Morgan", "Emily Bell", "Eric Murphy",
-        "Sophia Bailey", "Ryan Rivera", "Christine Cooper", "Jeffrey Richardson", "Amy Cox",
-        "Frank Howard", "Kathleen Ward", "Scott Torres", "Megan Peterson", "Raymond Gray",
-        "Laura Ramirez", "Gregory James", "Janet Watson", "Jacob Brooks", "Diane Price",
-        "Patrick Bennett", "Angela Wood", "Nicholas Barnes", "Cynthia Ross", "Joshua Henderson",
-        "Katherine Coleman", "Gary Jenkins", "Julie Perry", "Andrew Russell", "Victoria Butler",
-        "Justin Powell", "Teresa Foster", "Sean Simmons", "Carolyn Gonzales", "Stephen Bryant",
-        "Martha Alexander", "Benjamin Russell", "Frances Griffin", "Matthew Shaw", "Ann Knight",
-        "Larry Reynolds", "Pamela Fisher", "Timothy Webb", "Debra Kennedy", "Walter Welch",
-        "Janice Daniels", "Peter Stevens", "Ruth Wheeler", "Kyle Harper", "Jacqueline Berry",
-        "Dennis Warren", "Alice Elliott", "Raymond Fields", "Julia Bishop", "Roger Perry",
-        "Gloria Burns", "Jeremy Olson", "Victoria Hart", "Samuel Holland", "Marie Reynolds"
-    ]
-
-    
+    var contacts = [String]()
     var sectionTitle = [String]()
-    
     var searchContact = [String]()
-    
     var namesDict = [String: [String]]()
-    
-    
     var finalContacts = [String]()
-
-    
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
@@ -53,52 +16,42 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         searchBar.delegate = self
         
+        readJSON()
+        
         finalContacts = contacts
 
-        
         sectionTitle = Array(Set(finalContacts.compactMap({value in
             String(value.prefix(1))
         })))
         
         sectionTitle.sort()
         
-        for secTitle in sectionTitle{
+        for secTitle in sectionTitle {
             namesDict[secTitle] = [String]()
         }
         
-        for name in finalContacts{
+        for name in finalContacts {
             namesDict[String(name.prefix(1))]?.append(name)
         }
-
     }
-    
-
 }
 
-extension ViewController: UITableViewDelegate{
+extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("hello")
-        print(finalContacts[indexPath.row])
-        
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        
         let vc = storyBoard.instantiateViewController(identifier: "testVC")
-        
         vc.navigationItem.title = namesDict[sectionTitle[indexPath.section]]?[indexPath.row]
-        
         navigationController?.pushViewController(vc, animated: true)
     }
 }
 
-extension ViewController: UITableViewDataSource{
+extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return namesDict[sectionTitle[section]]?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        
         cell.textLabel?.text =  namesDict[sectionTitle[indexPath.section]]?[indexPath.row]
         return cell
     }
@@ -118,9 +71,7 @@ extension ViewController: UITableViewDataSource{
 
 extension ViewController: UISearchBarDelegate{
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-         searchContact = contacts.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
-        
-        
+        searchContact = contacts.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
         finalContacts = searchContact
         
         sectionTitle = Array(Set(finalContacts.compactMap({value in
@@ -129,16 +80,31 @@ extension ViewController: UISearchBarDelegate{
         
         sectionTitle.sort()
         
-        for secTitle in sectionTitle{
+        for secTitle in sectionTitle {
             namesDict[secTitle] = [String]()
         }
         
-        for name in finalContacts{
+        for name in finalContacts {
             namesDict[String(name.prefix(1))]?.append(name)
         }
-    
+        
         tableView.reloadData()
-        
-        
+    }
+    
+    func readJSON() {
+        if let path = Bundle.main.path(forResource: "Name", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(filePath: path), options: .mappedIfSafe)
+                let name = try JSONDecoder().decode([String].self, from: data)
+                
+                for item in name {
+                    contacts.append(item)
+                }
+            }catch {
+                print("Something wrong")
+            }
+        }else {
+            print("wrong name")
+        }
     }
 }
